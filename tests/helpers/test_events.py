ca8ff8dc3e12d6f549e7dd47b8ddca173dc0386e
Mattias.Poppe <poppe.mattias@gmail.com>
2022-01-18 16:05:17 +0100

    DEV-1897 Add tests for parsing message

---
+++ tests/helpers/test_events.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+import pytest
+
+from app.helpers.events import WatchfolderMessage, InvalidMessageException
+
+
+folder = os.path.join("tests", "resources", "watchfolder")
+
+
+def _load_resource(filename):
+    with open(os.path.join(folder, filename), "rb") as f:
+        contents = f.read()
+    return contents
+
+
+def test_message_valid():
+    event = WatchfolderMessage(_load_resource("message.json"))
+    assert event.cp_name == "CPFIELD"
+    assert event.flow_id == "FLOWFIELD"
+    essence_file = event.essence_file
+    assert essence_file.file_name == "file.mxf.xml"
+    assert essence_file.file_path == "/path/to/xml/file"
+    assert essence_file.md5 == "1"
+
+    xml_file = event.xml_file
+    assert xml_file.file_name == "file.mxf"
+    assert xml_file.file_path == "/path/to/essence/file"
+    assert xml_file.md5 == "5"
+
+
+def test_message_invalid_json():
+    with pytest.raises(InvalidMessageException) as e:
+        WatchfolderMessage(_load_resource("message_invalid.json"))
+    assert (
+        str(e.value)
+        == "Message is not valid JSON: 'Expecting value: line 1 column 1 (char 0)'"
+    )
+
+
+def test_message_missing_key():
+    with pytest.raises(InvalidMessageException) as e:
+        WatchFolderEvent(_load_resource("message_missing_cp_name.json"))
+    assert str(e.value) == "Missing mandatory key: 'cp_name'"
